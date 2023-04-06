82281ce47ca5c37b88da515572e849d4f4f59892
Reza <reza@pop-os.localdomain>
2023-04-06 18:59:41 +0600

    refactor: testing git

--- src/components/AllInputs/input1/Input1.tsx
+++ src/components/AllInputs/input1/Input1.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export interface IInput1 {}
 
 const Input1: React.FC<IInput1> = () => {
-  return <div>Input1</div>;
+  return <div>Input1 test</div>;
 };
 
 export default Input1;
